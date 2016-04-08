4358571fa723af4d0dcd43a5f364da281310203a
André Alcantara <andre.alcantara.nti@ufpe.br>
2016-04-08 13:11:40 -0300

    Exemplo pronto de forEach

---
+++ src/main/java/exemplos/ExemploForEach.java
@@ -0,0 +1,32 @@
+package exemplos;
+
+import java.util.List;
+
+import control.Controller;
+import model.Usuario;
+
+public class ExemploForEach {
+	private static List<Usuario> usuarios = Controller.todosUsuarios();
+	
+	
+	public static void main(String[] args) {
+		ExemploForEach.exemploAntigo();
+		ExemploForEach.exemploJava8();
+		ExemploForEach.exemploJava8ReferenceMethod();
+	}
+	
+	public static void exemploAntigo() {
+		for(Usuario u : usuarios){
+			System.out.println(u);
+		}
+	}
+	
+	public static void exemploJava8() {
+		usuarios.forEach(x -> System.out.println(x));
+	}
+	
+	public static void exemploJava8ReferenceMethod() {
+		usuarios.forEach(System.out::println);
+	}
+
+}
