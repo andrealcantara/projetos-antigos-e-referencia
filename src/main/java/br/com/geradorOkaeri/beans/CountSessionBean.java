79f6c75ac13a603118c3afd5d6bdebb4e60c118a
André Alcantara <andrealcant@gmail.com>
2017-01-05 01:02:22 -0300

    Adicionando as configs de CountSessionBean. Configurado

--- src/main/java/br/com/geradorOkaeri/beans/CountSessionBean.java
+++ src/main/java/br/com/geradorOkaeri/beans/CountSessionBean.java
@@ -2,8 +2,6 @@ package br.com.geradorOkaeri.beans;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
@@ -16,50 +14,36 @@ import br.com.geradorOkaeri.util.GeradorFacesContext;
 @SessionScoped
 public class CountSessionBean implements Serializable {
 	private static final long serialVersionUID = -4624310991195490856L;
-	private static final long FINAL_TIMER = 30000; // 30 segundos
-//			60000L;  // 1 minuto
-//			420000L; // 7 minutos
+	public static final long FINAL_TIMER =
+			// 30000; // 30 segundos
+			60000L; // 1 minuto
+	// 420000L; // 7 minutos
 
-	private long secondTimeExpire;
-	private long timeExpire;
+	private static final long COUNT_DOWN_TIMER = 30000;
+
 	private Post post;
 
 	@PostConstruct
 	public void init() {
 		this.post = new Post();
-		this.timeExpire = 1L;
-		this.resetTime();
-		this.calculateTimeLeft();
-	}
-	
-	public void activity(){
-		this.calculateTimeLeft();
-		this.resetTime();
-		this.calculateTimeLeft();
 	}
 
-	public void calculateTimeLeft() {
-		long timeleft = this.timeExpire - System.currentTimeMillis();
-		if (timeleft < 500L) {
-			secondTimeExpire = 0L;
-			try {
-				GeradorFacesContext.endSession("animes.xhtml");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("fim sessao");
-		} else  {
-			secondTimeExpire = timeleft / 1000L;
+	public void endSession() {
+		Post other = new Post();
+		this.post = other;
+		try {
+			GeradorFacesContext.redirect("");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	public void resetTime() {
-		System.out.println(this);
-		long timeleft = this.timeExpire - System.currentTimeMillis();
-		if(timeleft > 500L || this.timeExpire == 1L){
-			timeExpire = System.currentTimeMillis() + CountSessionBean.FINAL_TIMER;
-		}
+
+	public Long getTimeSessaoIdleMonitor() {
+		return CountSessionBean.FINAL_TIMER - CountSessionBean.COUNT_DOWN_TIMER;
+	}
+
+	public Long getCountDownTimer() {
+		return CountSessionBean.COUNT_DOWN_TIMER;
 	}
 
 	public Post getPost() {
@@ -68,13 +52,5 @@ public class CountSessionBean implements Serializable {
 
 	public void setPost(Post post) {
 		this.post = post;
-	}
-
-	public long getSecondTimeExpire() {
-		return secondTimeExpire;
-	}
-
-	public void setSecondTimeExpire(long secondTimeExpire) {
-		this.secondTimeExpire = secondTimeExpire;
 	}
 }
