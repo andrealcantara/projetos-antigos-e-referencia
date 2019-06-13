036b6b6c2e12f60ad4e77484b7a044345cfd6ede
Andre G O Alcantara <andrealcant@gmail.com>
2019-06-13 17:10:28 -0300

    First commit

---
+++ Estudo-Camel-Rest-Consumir-Rest/src/main/java/com/dev/six/rest/camel/study/cosumer/rest/study/model/TipoTransacao.java
@@ -0,0 +1,28 @@
+package com.dev.six.rest.camel.study.cosumer.rest.study.model;
+
+public enum TipoTransacao {
+    CREDITO(0),
+    DEBITO(1),
+    ACERTO(2),
+    DESCONHECIDO(100);
+
+    private int key;
+
+    TipoTransacao(int key) {
+        this.key = key;
+    }
+
+    public static TipoTransacao getByValor(int key) {
+        for (TipoTransacao tt : values()) {
+            if (tt.getKey() == key) {
+                return tt;
+            }
+        }
+        return TipoTransacao.DESCONHECIDO;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+}
